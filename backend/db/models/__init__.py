d8b624c6fb766fa174b60c42bb07fc018e4f436e
Akramov Tolqin <akramovtolqin0327@gmail.com>
2026-09-06 05:18:25 +0000

    feat(vuln): register vulnerability models

--- backend/db/models/__init__.py
+++ backend/db/models/__init__.py
@@ -5,6 +5,7 @@ from db.models.event import EventModel
 from db.models.agent import AgentModel
 from db.models.honeypot import HoneypotSessionModel
 from db.models.simulation import SimulationModel
+from db.models.vulnerability import AssetSoftwareModel, VulnerabilityModel, AssetVulnerabilityModel
 
 __all__ = [
     "UserORM",
@@ -14,4 +15,7 @@ __all__ = [
     "AgentModel",
     "HoneypotSessionModel",
     "SimulationModel",
+    "AssetSoftwareModel",
+    "VulnerabilityModel",
+    "AssetVulnerabilityModel",
 ]
